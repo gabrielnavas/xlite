bf4eab0fae4d55d8a10688e82957351da57b2068
navas <gabrielnavas@email.com>
2024-05-22 16:55:31 -0300

    init backend spring boot with docker-compose postgres

---
+++ users-posts/backend/src/test/java/api/posts/PostsApplicationTests.java
@@ -0,0 +1,13 @@
+package api.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
