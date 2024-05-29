ca8e3b757f0cd632073ee5efce3a63dd6e5109ad
navas <gabrielnavas@email.com>
2024-05-29 17:45:27 -0300

    redirect to login, if it not auth

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 import { Container, styled } from "@mui/material";
 import Left from "../components/Home/Left";
 import Right from "../components/Home/Right";
+import localAuthManager from "../services/LocalAuthManager";
+import { useNavigate } from "react-router-dom";
+import { routePaths } from "../Router";
 
 
 const Page = styled(Container)(({theme}) => ({
@@ -14,9 +17,18 @@ const Page = styled(Container)(({theme}) => ({
 
 
 const Home = () => {
+  const navigate = useNavigate()
+  
   useEffect(() => {
     document.title = "Feed | Xlite";
   }, []);
+
+  useEffect(() => {
+    if(!localAuthManager().isAuth()) {
+      navigate(routePaths.auth.login)
+    }
+    document.title = "Feed | Xlite";
+  }, [navigate]);
 
   return (
     <Page>
