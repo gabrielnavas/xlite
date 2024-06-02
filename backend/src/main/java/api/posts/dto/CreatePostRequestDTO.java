727a1a1a4f9275706ec3621791da3fdb0d0065a7
navas <gabrielnavas@email.com>
2024-06-01 22:03:28 -0300

    create post with 2 characters

--- backend/src/main/java/api/posts/dto/CreatePostRequestDTO.java
+++ backend/src/main/java/api/posts/dto/CreatePostRequestDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record CreatePostRequestDTO(
         @NotBlank
-        @Size(min = 3, max = 100, message = "The length of description must be between 2 and 255 characters.")
+        @Size(min = 2, max = 100, message = "The length of description must be between 2 and 255 characters.")
         String description
 ) {
 }
